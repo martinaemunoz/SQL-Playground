d326492b6d6007c8243ff693beee14249227e6f9
Martina Muñoz <martinaemunoz@gmail.com>
2024-11-28 22:29:18 -0300

    Add more  multi-table exercises with JOINs

--- sample_db/sample_db_joins.sql
+++ sample_db/sample_db_joins.sql
@@ -2,12 +2,15 @@
 
 SELECT CONCAT(e.nombre, ' ', e.apellido) AS nombre_apellido, SUM(v.cantidad) AS cantidad_total
 FROM empleados e
-JOIN ventas v ON e.id = v.empleado_id
+INNER JOIN ventas v ON e.id = v.empleado_id
 GROUP BY nombre_apellido;
 
 -- Calcula el monto total vendido a cada cliente y muestra el nombre del cliente, su dirección y el monto total.
 
-SELECT c.nombre AS nombre_cliente, c.direccion AS direccion_cliente, SUM(v.monto_total) AS monto_total 
+SELECT 
+    c.nombre AS nombre_cliente, 
+    c.direccion AS direccion_cliente, 
+    SUM(v.monto_total) AS monto_total 
 FROM ventas v
 JOIN clientes c ON v.cliente_id = c.id
 GROUP BY c.nombre, c.direccion;
@@ -24,9 +27,43 @@ GROUP BY e.nombre, p.nombre;
 
 -- Encuentra el nombre del cliente, el nombre del producto y la cantidad comprada de productos con un precio superior a $500.
 
-SELECT c.nombre AS nombre_cliente, p.nombre AS nombre_producto, SUM(v.cantidad) AS cantidad_total, p.precio AS precio_mayor_500
+SELECT 
+    c.nombre AS nombre_cliente, 
+    p.nombre AS nombre_producto, 
+    SUM(v.cantidad) AS cantidad_total, 
+    p.precio AS precio_mayor_500
 FROM ventas v
 JOIN clientes c ON v.cliente_id = c.id
 JOIN productos p ON v.producto_id = p.id
 WHERE p.precio > 500
 GROUP BY c.nombre, p.nombre, p.precio;
+
+-- Calcula la cantidad de ventas por departamento, incluso si el departamento no tiene ventas.
+
+SELECT d.nombre AS nombre_departamento, SUM(v.cantidad) AS cant_total
+FROM departamentos d
+LEFT JOIN empleados e ON d.id = e.departamento_id
+LEFT JOIN ventas v ON e.id = v.empleado_id
+GROUP BY d.nombre;
+
+-- Encuentra el nombre y la dirección de los clientes que han comprado más de 3 productos distintos y muestra la cantidad de productos comprados.
+
+SELECT
+    c.nombre AS nombre_cliente, 
+    c.direccion as direccion_cliente, 
+    COUNT(DISTINCT p.id) AS productos_distintos, 
+    SUM(v.cantidad) AS cantidad_total
+FROM clientes c
+JOIN ventas v ON v.cliente_id = c.id
+JOIN productos p ON v.producto_id = p.id
+GROUP BY c.nombre, c.direccion
+HAVING COUNT(DISTINCT p.id) > 3;
+
+-- Calcula el monto total de ventas realizadas por cada departamento y muestra el nombre del departamento junto con el monto total de ventas.
+
+
+SELECT d.nombre AS nombre_departamento, SUM(v.monto_total) AS monto_total
+FROM departamentos d
+JOIN empleados e ON d.id = e.departamento_id
+JOIN ventas v ON e.id = v.empleado_id
+GROUP BY d.nombre;
